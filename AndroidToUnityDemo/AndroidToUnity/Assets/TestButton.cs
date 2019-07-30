625275673da3d8813784d80d54c79f216b712fa8
WIN-0PVOGV6CLCP\Administrator <314284230@qq.com>
2019-07-30 17:55:42 +0800

    提交AndroidToUnityDemo。提交QQSDK。提交对应学习资料

---
+++ AndroidToUnityDemo/AndroidToUnity/Assets/TestButton.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TestButton : MonoBehaviour {
+
+    [SerializeField]
+    Button button;
+    // Use this for initialization
+
+    [SerializeField]
+    Text text;
+
+	void Start () {
+        button.onClick.AddListener(() => {
+            CallAndroid();
+        });
+	}
+	
+
+    void CallAndroid()
+    {
+        AndroidJavaClass jc = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+        AndroidJavaObject jo = jc.GetStatic<AndroidJavaObject>("Callunity");
+
+        string str = jo.Call<string>("UnityCallAndroid", "大帅哥");
+
+        text.text = str;
+    }
+
+}
